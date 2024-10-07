f4d139191b875b590f768c2905c8efb2b071e117
yudi009p <133029592+yudi009p@users.noreply.github.com>
2024-10-07 11:47:03 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Matemática',
+    'Qual é a soma de 45 e 78?',
+    '123'
+)
+
+criaCartao(
+    'Matemática',
+    'Se um triângulo tem lados de 3, 4 e 5, qual é o tipo desse triângulo?',
+    'Escaleno'
+)
+
+criaCartao(
+    'Matemática',
+    'Quanto é 70 horas tranformadas em minutos ',
+    '4200 minutos '
+)
+
+criaCartao(
+    'Matemática',
+    'Quanto vale 10% de 2569 reais',
+    '256,90'
+)
